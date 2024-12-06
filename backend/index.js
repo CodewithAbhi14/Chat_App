837e8b82185902836c99e6d51324a826bb7c1b0b
Abhishek <147073711+CodewithAbhi14@users.noreply.github.com>
2024-12-06 18:05:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT;
 const __dirname = path.resolve()
 
 
-app.use(express.json());
+app.use(express.json({limit: "100mb"}));
 app.use(cookieParser());
 app.use(
   cors({
